936acf3980155b6d2d3dea705ccbb0cc72a66fa6
Vishal2690 <vishalc2690@gmail.com>
2023-01-02 14:34:08 +0530

    created all

---
+++ index.js
@@ -0,0 +1,25 @@
+let studentRecords = [ {name: 'John', id: 123, marks : 98},
+          {name: 'Baba', id: 101, marks : 23 },
+          {name: 'yaga', id: 200, marks : 45 },
+          {name: 'Wick', id: 115, marks : 75 }];
+
+
+            let res=[];
+          for(let i=0; i<studentRecords.length; i++){
+            let obj=studentRecords[i];
+                res.push(obj.name.toUpperCase());
+          }
+          console.log(res);
+
+
+
+          
+            let res=[];
+          for(let i=0; i<studentRecords.length; i++){
+            let obj=studentRecords[i];
+                res.push(obj.name.toUpperCase());
+          }
+          console.log(res);
+
+
+
